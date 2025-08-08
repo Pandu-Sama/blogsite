cf74e2ce7cd7877c6ffd683306b55083f0369f79
PanduSama <pandurosama@gmail.com>
2025-08-07 22:03:09 -0300

    Registrar y personalizar modelos en el admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from .models import Author, Post, Category
 
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 1 #Nùmero de formularios vacìos para añadir posts
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'email')
     search_fields = ('name', 'email')
+    list_filter = ('name',)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'author', 'fecha_publicacion')
     list_filter = ('author', 'fecha_publicacion', 'categories')
     search_fields = ('titulo', 'contenido')
+    date_hierarchy = 'fecha_publicacion'
+    ordering = ('-fecha_publicacion',)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
